185679521172ff70c825a6434c80991da4807023
Collins Mwenda <mwendacollinsgakii@gmail.com>
2024-03-23 10:41:52 +0300

    Deletion

--- Hackathon1/question1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  String name = "Collins Mwenda";
-  int age = 19;
-  String school = "University of Nairobi";
-  String hobby = "Tech";
-  
-  print("MDy name is $name. I am $age years old, and I study at the $school. My hobby is $hobby.");
-}
